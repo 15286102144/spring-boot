eeead9ba14c20851883e169743c382c89486a9a8
15286102144 <425741480@qq.com>
2020-08-10 19:22:30 +0800

    "全局配置文件以接口方式运行+日志系统已完成"

--- src/main/java/com/hqyj/javaSpringBoot/models/test/controller/TestController.java
+++ src/main/java/com/hqyj/javaSpringBoot/models/test/controller/TestController.java
@@ -1,5 +1,10 @@
 package com.hqyj.javaSpringBoot.models.test.controller;
 
+import com.hqyj.javaSpringBoot.models.test.vo.ApplicationTest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +13,52 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/test")
 public class TestController {
+
+    private final static Logger LOGGER =LoggerFactory.getLogger(TestController.class);
+    @Value("${server.port}")
+    private int port;
+    @Value("${com.name}")
+    private String name;
+    @Value("${com.age}")
+    private int age;
+    @Value("${com.desc}")
+    private String desc;
+    @Value("${com.random}")
+    private String random;
+
+
+
+@GetMapping("/logtest")
+@ResponseBody
+    public  String logtest(){
+        LOGGER.trace("trace");
+        LOGGER.debug("debug");
+        LOGGER.info("info");
+        LOGGER.warn("warn");
+        LOGGER.error("error");
+        return "logtest";
+    }
+@GetMapping("/config")
+@ResponseBody
+    public String configtest(){
+    StringBuffer s=new StringBuffer();
+                s
+                .append(port).append("-----")
+                .append(name).append("-----")
+                .append(age).append("-----")
+                .append(desc).append("-----")
+                .append(random).append("-----").append("<br>");
+               s
+                .append(applicationTest.getPort()).append("-----")
+                .append(applicationTest.getName()).append("-----")
+                .append(applicationTest.getAge()).append("-----")
+                .append(applicationTest.getDesc()).append("-----")
+                .append(applicationTest.getRandom()).append("-----").append("<br>");
+               return s.toString();
+    }
+
+@Autowired
+    private ApplicationTest applicationTest;
     //git方式请求
     @GetMapping("demo1")
     @ResponseBody
