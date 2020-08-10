c9cb5c792ccc389e00c8a2e3e1b25b64ca40e2b9
15286102144 <425741480@qq.com>
2020-08-10 12:56:33 +0800

    "第一次创建项目并提交"

---
+++ src/main/java/com/hqyj/javaSpringBoot/models/test/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.hqyj.javaSpringBoot.models.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/test")
+public class TestController {
+    @GetMapping("demo1")
+    @ResponseBody
+    public String text(){
+        return "djfsdifidsfidsfsdfsdk";
+    }
+}
+
